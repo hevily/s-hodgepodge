b913aa0865497f0cbdc5f75d454c58f8abe166ef
stone <stone0090@qq.com>
2016-05-22 15:32:38 +0800

    优化tab.js，兼容全平台（包含ie6+）。

--- web/tab/tab.js
+++ web/tab/tab.js
@@ -107,6 +107,24 @@
             transformOrigin: me.prefixStyle('transformOrigin')
         });
 
+        me.extend(me.eventType = {}, {
+            touchstart: 1,
+            touchmove: 1,
+            touchend: 1,
+
+            mousedown: 2,
+            mousemove: 2,
+            mouseup: 2,
+
+            pointerdown: 3,
+            pointermove: 3,
+            pointerup: 3,
+
+            MSPointerDown: 3,
+            MSPointerMove: 3,
+            MSPointerUp: 3
+        });
+
         me.hasClass = function(e, c) {
             var re = new RegExp("(^|\\s)" + c + "(\\s|$)");
             return re.test(e.className);
@@ -169,6 +187,7 @@
         }
 
         this.touch = {};
+        this._events = {};
 
         this.opts = {
             currentClassName: 'current',
@@ -187,7 +206,7 @@
         this.index = this.oldIndex = this.opts.index || 0;
 
         this._init();
-    }
+    };
 
     Tab.prototype = {
         _init: function() {
@@ -214,10 +233,13 @@
             for (var i = 0; i < this.length; i++) {
                 this.contents[i].style.width = this.width + 'px';
             }
-            this.content.style[utils.prefixStyle('transform')] = 'translateX(' + (-this.index * this.width) + 'px)';
+            // this._translate();
         },
         _resize: function() {
-            this._initSize();
+            var me = this;
+            window.setTimeout(function() {
+                me._initSize();
+            }, 100);
         },
         _initClass: function() {
             if (this.opts.useDefualtCSS) {
@@ -232,62 +254,219 @@
             this.menus[this.index].className += ' ' + this.opts.currentClassName;
             this.contents[this.index].className += ' ' + this.opts.currentClassName;
         },
-        _initEvent: function() {
+        _initEvent: function(remove) {
+            var handler = remove ? utils.removeHandler : utils.addHandler;
             var me = this;
-            utils.addHandler(this.wrap, 'touchstart', function(e) {
+
+            handler(window, 'resize', function(e) {
+                me._resize(e);
+            });
+            handler(window, 'orientationchange', function(e) {
+                me._resize(e);
+            });
+
+            if (utils.hasTouch) {
+                handler(this.wrap, 'touchstart', function(e) {
+                    me._touchStart(e);
+                });
+                handler(this.wrap, 'touchmove', function(e) {
+                    me._touchMove(e);
+                });
+                handler(this.wrap, 'touchcancel', function(e) {
+                    me._touchEnd(e);
+                });
+                handler(this.wrap, 'touchend', function(e) {
+                    me._touchEnd(e);
+                });
+            }
+
+            if (utils.hasPointer) {
+                handler(this.wrap, utils.prefixPointerEvent('pointerdown'), function(e) {
+                    me._touchStart(e);
+                });
+                handler(this.wrap, utils.prefixPointerEvent('pointermove'), function(e) {
+                    me._touchMove(e);
+                });
+                handler(this.wrap, utils.prefixPointerEvent('pointercancel'), function(e) {
+                    me._touchEnd(e);
+                });
+                handler(this.wrap, utils.prefixPointerEvent('pointerup'), function(e) {
+                    me._touchEnd(e);
+                });
+            }
+
+            handler(this.wrap, 'mousedown', function(e) {
                 me._touchStart(e);
             });
-            utils.addHandler(this.wrap, 'touchmove', function(e) {
+            handler(this.wrap, 'mousemove', function(e) {
                 me._touchMove(e);
             });
-            utils.addHandler(this.wrap, 'touchend', function(e) {
+            handler(this.wrap, 'mousecancel', function(e) {
                 me._touchEnd(e);
             });
-            utils.addHandler(this.wrap, 'touchcancel', function(e) {
+            handler(this.wrap, 'mouseup', function(e) {
                 me._touchEnd(e);
             });
-            utils.addHandler(this.menu, 'click', function(e) {
+
+            handler(this.menu, 'click', function(e) {
                 me._touchClick(e);
             });
-            utils.addHandler(this.content, utils.prefixHandler('transitionEnd'), function(e) {
+            handler(this.menu, 'touchend', function(e) {
+                me._touchClick(e);
+            });
+            handler(this.content, utils.prefixHandler('transitionEnd'), function(e) {
                 me._transitionEnd(e);
             });
-            utils.addHandler(window, 'resize', function() {
-                window.setTimeout(function() {
-                    me._resize();
-                }, 100);
-            });
-            utils.addHandler(window, 'orientationchange', function() {
-                window.setTimeout(function() {
-                    me._resize();
-                }, 100);
-            });
         },
+        // // ie6 ~ ie8 not support  
+        // _initEvent: function(remove) {
+        //     var handler = remove ? utils.removeHandler : utils.addHandler;
+
+        //     handler(window, 'resize', this);
+        //     handler(window, 'orientationchange', this);
+
+        //     if (utils.hasTouch) {
+        //         handler(this.wrap, 'touchstart', this);
+        //         handler(this.wrap, 'touchmove', this);
+        //         handler(this.wrap, 'touchcancel', this);
+        //         handler(this.wrap, 'touchend', this);
+        //     }
+
+        //     if (utils.hasPointer) {
+        //         handler(this.wrap, utils.prefixPointerEvent('pointerdown'), this);
+        //         handler(this.wrap, utils.prefixPointerEvent('pointermove'), this);
+        //         handler(this.wrap, utils.prefixPointerEvent('pointercancel'), this);
+        //         handler(this.wrap, utils.prefixPointerEvent('pointerup'), this);
+        //     }
+
+        //     handler(this.wrap, 'mousedown', this);
+        //     handler(this.wrap, 'mousemove', this);
+        //     handler(this.wrap, 'mousecancel', this);
+        //     handler(this.wrap, 'mouseup', this);
+
+        //     handler(this.menu, 'click', this);
+        //     handler(this.content, utils.prefixHandler('transitionEnd'), this);
+        // },
+        // handleEvent: function(e) {
+        //     switch (e.type) {
+        //         case 'touchstart':
+        //         case 'pointerdown':
+        //         case 'MSPointerDown':
+        //         case 'mousedown':
+        //             this._touchStart(e);
+        //             break;
+        //         case 'touchmove':
+        //         case 'pointermove':
+        //         case 'MSPointerMove':
+        //         case 'mousemove':
+        //             this._touchMove(e);
+        //             break;
+        //         case 'touchend':
+        //         case 'pointerup':
+        //         case 'MSPointerUp':
+        //         case 'mouseup':
+        //         case 'touchcancel':
+        //         case 'pointercancel':
+        //         case 'MSPointerCancel':
+        //         case 'mousecancel':
+        //             this._touchEnd(e);
+        //             break;
+        //         case 'transitionend':
+        //         case 'webkitTransitionEnd':
+        //         case 'oTransitionEnd':
+        //         case 'MSTransitionEnd':
+        //             this._transitionEnd(e);
+        //             break;
+        //         case 'click':
+        //             this._touchClick(e);
+        //             break;
+        //         case 'orientationchange':
+        //         case 'resize':
+        //             this._resize();
+        //             break;
+        //     }
+        // },
         _touchStart: function(e) {
-            this.touch.x = e.touches[0].pageX;
-            this.touch.y = e.touches[0].pageY;
+            // React to left mouse button only
+            if (utils.eventType[e.type] != 1) {
+                // for button property
+                // http://unixpapa.com/js/mouse.html
+                var button;
+                if (!e.which) {
+                    button = (e.button < 2) ? 0 : ((e.button == 4) ? 1 : 2); // IE case 
+                } else {
+                    button = e.button; // All others
+                }
+                if (button !== 0) {
+                    return;
+                }
+            }
+
+            if (this.initiated && utils.eventType[e.type] !== this.initiated) {
+                return;
+            }
+
+            // ie6 ~ ie8 not support e.preventDefault
+            if (e.preventDefault) {
+                e.preventDefault();
+            } else {
+                e.returnValue = false;
+            }
+
+            this.initiated = utils.eventType[e.type];
+
+            // ie6 ~ ie8 not support e.pageX
+            var point = e.touches ? e.touches[0] : e;
+            var pageX = (point.pageX) ? point.pageX :
+                e.clientX + (document.documentElement.scrollLeft ?
+                    document.documentElement.scrollLeft :
+                    document.body.scrollLeft);
+            var pageY = (point.pageY) ? point.pageY :
+                e.clientY + (document.documentElement.scrollTop ?
+                    document.documentElement.scrollTop :
+                    document.body.scrollTop);
+
+            this.touch.x = pageX;
+            this.touch.y = pageY;
             this.touch.time = Date.now();
             this.touch.disX = 0;
             this.touch.disY = 0;
             this.touch.fixed = '';
         },
         _touchMove: function(e) {
+            if (utils.eventType[e.type] !== this.initiated) {
+                return;
+            }
+
             if (this.touch.fixed === 'up') {
+                this.initiated = false;
                 return;
             }
 
-            // ie6 ~ ie8 not support e.stopPropagation
-            if (e.stopPropagation) {
-                e.stopPropagation();
+            // ie6 ~ ie8 not support e.preventDefault
+            if (e.preventDefault) {
+                e.preventDefault();
             } else {
-                e.cancelBubble = false;
+                e.returnValue = false;
             }
-            if (e.touches.length > 1 || e.scale && e.scale !== 1) {
+
+            // ie6 ~ ie8 not support e.pageX
+            var point = e.touches ? e.touches[0] : e;
+            var pageX = (point.pageX) ? point.pageX :
+                e.clientX + (document.documentElement.scrollLeft ?
+                    document.documentElement.scrollLeft :
+                    document.body.scrollLeft);
+            var pageY = (point.pageY) ? point.pageY :
+                e.clientY + (document.documentElement.scrollTop ?
+                    document.documentElement.scrollTop :
+                    document.body.scrollTop);
+
+            if (point.length > 1 || e.scale && e.scale !== 1) {
                 return;
             }
 
-            this.touch.disX = e.touches[0].pageX - this.touch.x;
-            this.touch.disY = e.touches[0].pageY - this.touch.y;
+            this.touch.disX = pageX - this.touch.x;
+            this.touch.disY = pageY - this.touch.y;
             if (this.touch.fixed === '') {
                 if (Math.abs(this.touch.disY) > Math.abs(this.touch.disX)) {
                     this.touch.fixed = 'up';
@@ -296,22 +475,37 @@
                 }
             }
             if (this.touch.fixed === 'left') {
-                // ie6 ~ ie8 not support e.preventDefault
-                if (e.preventDefault) {
-                    e.preventDefault();
-                } else {
-                    e.returnValue = false;
-                }
                 if ((this.index === 0 && this.touch.disX > 0) || (this.index === this.length - 1 && this.touch.disX < 0)) {
                     this.touch.disX /= 4;
                 }
-                this.content.style[utils.prefixStyle('transform')] = 'translateX(' + (this.touch.disX - this.index * this.width) + 'px)';
+
+                this._translate(this.touch.disX - this.index * this.width, true);
             }
         },
         _touchEnd: function(e) {
+
+            if (!this.initiated) {
+                return;
+            }
+
+            this.initiated = false;
+
+            // ie6 ~ ie8 not support e.preventDefault
+            if (e.preventDefault) {
+                e.preventDefault();
+            } else {
+                e.returnValue = false;
+            }
+
+            // ie6 ~ ie8 not support e.stopPropagation
+            if (e.stopPropagation) {
+                e.stopPropagation();
+            } else {
+                e.cancelBubble = false;
+            }
+
             if (this.touch.fixed === 'left') {
                 var absX = Math.abs(this.touch.disX);
-                this.content.style[utils.prefixStyle('transition')] = 'all ' + this.opts.duration + 'ms';
                 if ((Date.now() - this.touch.time > 100 && absX > 10) || absX > this.width / 2) {
                     this.touch.time = Date.now();
                     if (this.touch.disX > 0) {
@@ -325,21 +519,20 @@
                     if (this.index > this.length - 1) {
                         this.index = this.length - 1;
                     }
-                    if (this.index === this.oldIndex) {
-                        this.content.style[utils.prefixStyle('transition')] = 'all ' + this.opts.duration + 'ms';
-                    }
                     if (this.index !== this.oldIndex) {
                         this._replace();
                     }
                 }
-                this.content.style[utils.prefixStyle('transform')] = 'translateX(' + (-this.index * this.width) + 'px)';
+
+                this._translate();
+
                 if (this.opts.callback[this.index]) {
                     this.opts.callback[this.index]();
                 }
             }
         },
         _transitionEnd: function() {
-            this.content.style[utils.prefixStyle('transition')] = 'all 0ms';
+            this._execEvent('scrollEnd');
         },
         _touchClick: function(e) {
             var target = e.target || e.srcElement;
@@ -363,8 +556,8 @@
                 }
 
                 this.index = target.index;
-                this.content.style[utils.prefixStyle('transition')] = 'all ' + this.opts.duration + 'ms';
-                this.content.style[utils.prefixStyle('transform')] = 'translateX(' + (-this.index * this.width) + 'px)';
+                this._translate();
+
                 if (this.opts.callback[this.index]) {
                     this.opts.callback[this.index]();
                 }
@@ -378,31 +571,67 @@
             this.contents[this.oldIndex].className = this.contents[this.oldIndex].className.replace(this.opts.currentClassName, '').trim();
             this.oldIndex = this.index;
         },
-        _translate: function(x, y) {
-            if (utils.hasTransform) {
-                /* REPLACE START: _translate */
-                this.scrollerStyle[utils.style.transform] = 'translate(' + x + 'px,' + y + 'px)' + this.translateZ;
-                /* REPLACE END: _translate */
+        _translate: function(moveX) {
+            var destX = moveX || (-this.index * this.width);
+            if (utils.hasTransform && utils.hasTransition) {
+                this.content.style[utils.prefixStyle('transform')] = 'translateX(' + destX + 'px)';
+                this.content.style[utils.prefixStyle('transition')] = 'all ' + this.opts.duration + 'ms';
             } else {
-                x = Math.round(x);
-                y = Math.round(y);
-                this.scrollerStyle.left = x + 'px';
-                this.scrollerStyle.top = y + 'px';
-            }
-
-            this.x = x;
-            this.y = y;
-
-            if (this.indicators) {
-                for (var i = this.indicators.length; i--;) {
-                    this.indicators[i].updatePosition();
+                this.content.style.left = destX + 'px';
+                if (moveX) {
+                    this.x = moveX;
+                } else {
+                    this._updatePosition();
                 }
             }
-            // INSERT POINT: _translate
+        },
+        _updatePosition: function() {
+            var me = this,
+                startX = me.x || 0,
+                destX = -this.index * this.width,
+                duration = this.opts.duration,
+                startTime = utils.getTime(),
+                destTime = startTime + duration;
+
+            function step() {
+                var now = utils.getTime(),
+                    newX, newY;
+
+                if (now >= destTime || (startX === destX)) {
+                    me.isAnimating = false;
+                    me.x = destX;
+                    me.content.style.left = destX + 'px';
+                    return;
+                }
+
+                now = (now - startTime) / duration;
+                newX = (destX - startX) * now + startX;
+                me.content.style.left = newX + 'px';
+
+                if (me.isAnimating) {
+                    rAF(step);
+                }
+            }
+
+            me.isAnimating = true;
+            step();
+        },
+        _execEvent: function(type) {
+            if (!this._events[type]) {
+                return;
+            }
+            var i = 0,
+                l = this._events[type].length;
+
+            if (!l) {
+                return;
+            }
+
+            for (; i < l; i++) {
+                this._events[type][i].apply(this, [].slice.call(arguments, 1));
+            }
         }
     };
-
-
 
     window.Tab = Tab;
 
